5fb8e012b7b481f72abd7a2f4a31407c68fc895e
Philip Sultanescu <philip@firstorder.io>
2020-04-13 21:09:54 +0200

    feat(app): add geotiff
    To ger the error "export 'default' (imported as 'GeoTiff') was not found in 'geotiff' open a terminal and run:
    `npm i && npm run build:ssr`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import GeoTiff from 'geotiff';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+  title = 'geotiff demo';
+  pool = new GeoTiff.Pool();
 }
